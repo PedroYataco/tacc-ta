22729f75aad0222a3b2577b2803bab5e1307fcb1
BurstT <89946525+BurstT@users.noreply.github.com>
2024-07-05 02:31:21 -0500

    U20123212
    ultima función agregada

--- Despliegue/Back/app/generate_story.py
+++ Despliegue/Back/app/generate_story.py
@@ -35,6 +35,38 @@ def generate_story(prompt, max_length=512, num_return_sequences=1):
     capitalized_stories = [capitalize_sentences(story) for story in stories]
     return capitalized_stories
 
+# Función para reformatear el texto
+def reformat_text(text):
+    paragraphs = text.split('\n\n')
+    formatted_paragraphs = [paragraph.strip().capitalize() for paragraph in paragraphs]
+    return '\n\n'.join(formatted_paragraphs)
+
+# Función para generar texto
+def generate_story_v2(prompt, max_length=512, num_return_sequences=1, temperature=0.7, top_k=50, model_path='./results'):
+    # Cargar el tokenizer y el modelo desde el directorio especificado
+    tokenizer = GPT2TokenizerFast.from_pretrained(model_path)
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+
+    # Tokenizar el prompt de entrada
+    inputs = tokenizer(prompt, return_tensors='pt')
+    
+    # Generar texto a partir del modelo
+    outputs = model.generate(
+        inputs['input_ids'],
+        max_length=max_length,
+        num_return_sequences=num_return_sequences,
+        pad_token_id=tokenizer.eos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        do_sample=True,  # Para generar texto de manera más creativa
+        temperature=temperature,  # Controla la creatividad del texto
+        top_k=top_k  # Para limitar el número de palabras a considerar para cada paso
+    )
+
+    # Decodificar las secuencias generadas y capitalizar las oraciones
+    stories = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
+    reformatted_stories = [reformat_text(story) for story in stories]
+    
+    return reformatted_stories
 
 # if __name__ == '__main__':
 #     prompt = "moatian ronki ipaonike"
